3041eccd84fa4e2a8781642bc940a240d545180a
Andrei M <andreimartinenco.dev@gmail.com>
2025-04-19 16:08:39 -0400

    (wip): vao and 2d/3d object distinction

---
+++ scripts/utils/constants.js
@@ -0,0 +1,4 @@
+export const ObjectType = Object.freeze({
+    TWO_D: "2D",
+    THREE_D: "3D"
+});
--- scripts/webgl/object.js
+++ scripts/webgl/object.js
@@ -1,21 +1,56 @@
 import { createVertexBuffer } from './buffer-manager.js';
 
-export class Object3D {
-    constructor(gl, vertices, shaderProgram) {
+export class ObjectBase {
+    constructor(gl, vertices, shaderProgram, attributeSize) {
         this.gl = gl;
+        this.vertices = vertices;
         this.vertexBuffer = createVertexBuffer(gl, vertices);
         this.shaderProgram = shaderProgram;
+        this.attributeSize = attributeSize;
+
+        this.vao = gl.createVertexArray();
+        gl.bindVertexArray(this.vao);
+
+        this.setupAttributes();
+
+        gl.bindVertexArray(null);
+    }
+
+    setupAttributes() {
+        const gl = this.gl;
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        const positionAttributeLocation = gl.getAttribLocation(this.shaderProgram, "a_position");
+        gl.enableVertexAttribArray(positionAttributeLocation);
+        gl.vertexAttribPointer(positionAttributeLocation, this.attributeSize, gl.FLOAT, false, 0, 0);
     }
 
     draw() {
         const gl = this.gl;
-        gl.useProgram(this.shaderProgram);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.vertexBuffer);
+        gl.bindVertexArray(this.vao);
+        this.render();
+        gl.bindVertexArray(null);
+    }
 
-        const positionAttributeLocation = gl.getAttribLocation(this.shaderProgram, "a_position");
-        gl.enableVertexAttribArray(positionAttributeLocation);
-        gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+    render() {
+    }
+}
 
-        gl.drawArrays(gl.TRIANGLES, 0, 3); // Assuming a triangle for now
+export class Object3D extends ObjectBase {
+    constructor(gl, vertices, shaderProgram) {
+        super(gl, vertices, shaderProgram, 3);
+    }
+
+    render() {
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, this.vertices.length / 3);
+    }
+}
+
+export class Object2D extends ObjectBase {
+    constructor(gl, vertices, shaderProgram) {
+        super(gl, vertices, shaderProgram, 2);
+    }
+
+    render() {
+        this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, this.vertices.length / 2);
     }
 }
